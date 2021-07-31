0ce0e351342c67a99be6ffb84b2faf766f658e60
panghu <panghu@0125>
2021-07-31 20:25:12 +0800

    测试2

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const app = require("./app/index")
 const { APP_PORT } = require("./app/config")
 require('./app/database')
 
-//注释
+//注释2
 app.listen(APP_PORT, () => {
     console.log(`服务器端口号为${APP_PORT}启动成功`);
 })
